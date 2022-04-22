94e4e97fcdea2bcea527f05660377db9dab5bd08
Biolexey <Biolexey@gmail.com>
2022-04-22 16:05:51 +0900

    4/22

---
+++ ImageCompress.py
@@ -0,0 +1,15 @@
+import os
+import glob
+from PIL import Image
+
+dst_dir = 'data/temp/images_compressd'
+os.makedirs(dst_dir, exist_ok=True)
+
+files = glob.glob('./data/temp/images/*.png')
+
+for f in files:
+    img = Image.open(f)
+    img_resize = img.resize((240, 135))
+    root, ext = os.path.splitext(f)
+    basename = os.path.basename(root)
+    img_resize.save(os.path.join(dst_dir, basename + ext))
